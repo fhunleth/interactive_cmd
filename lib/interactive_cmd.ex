916ea284f8bda111cea095946beb7cd2c03af81a
Frank Hunleth <fhunleth@troodon-software.com>
2025-11-26 10:18:13 -0500

    Debug

--- lib/interactive_cmd.ex
+++ lib/interactive_cmd.ex
@@ -54,11 +54,14 @@ defmodule InteractiveCmd do
 
     send(:user_drv, {self(), {:open_editor, ""}})
 
+    IO.puts("WAITING!!!!")
+
     result =
       receive do
         {_pid, {:editor_data, output}} -> output
       end
 
+    IO.puts("BACK!!!!!")
     File.cd!(original_dir)
     restore_env(original_env)
     {"", parse_exit_status(result)}
