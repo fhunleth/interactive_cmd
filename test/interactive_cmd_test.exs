5f74a36a2de9f417ebb46c2869f4af553a012743
Frank Hunleth <fhunleth@troodon-software.com>
2025-11-26 11:59:46 -0500

    sleeps

--- test/interactive_cmd_test.exs
+++ test/interactive_cmd_test.exs
@@ -6,92 +6,95 @@ defmodule InteractiveCmdTest do
   use ExUnit.Case, async: false
   doctest InteractiveCmd
 
-  describe "interactive_cmd/3" do
-    test "runs a command" do
-      path = "tmp_file.txt"
+  setup do
+    Process.sleep(1000)
+    :ok
+  end
 
-      _ = File.rm(path)
-      {"", 0} = InteractiveCmd.cmd("touch", [path])
+  test "runs a command" do
+    path = "tmp_file.txt"
 
-      assert File.exists?(path)
-      File.rm!(path)
-    end
+    _ = File.rm(path)
+    {"", 0} = InteractiveCmd.cmd("touch", [path])
 
-    test "returns exit status" do
-      # Spot check that return values pass through
-#      statuses = [0, 1, 2, 3, 4, 5, 6, 7, 127, 128, 255]
+    assert File.exists?(path)
+    File.rm!(path)
+  end
 
-#      Enum.each(statuses, fn i ->
-#        {"", status} = InteractiveCmd.cmd("./test/fixture/exit_code.sh", [to_string(i)])
-#        assert status == i
-#      end)
-    end
+  test "returns exit status" do
+    # Spot check that return values pass through
+    #      statuses = [0, 1, 2, 3, 4, 5, 6, 7, 127, 128, 255]
 
-    test "paths with spaces" do
-      path = "tmp_file with spaces.txt"
+    #      Enum.each(statuses, fn i ->
+    #        {"", status} = InteractiveCmd.cmd("./test/fixture/exit_code.sh", [to_string(i)])
+    #        assert status == i
+    #      end)
+  end
 
-      _ = File.rm(path)
-      {"", 0} = InteractiveCmd.cmd("touch", [path])
+  test "paths with spaces" do
+    path = "tmp_file with spaces.txt"
 
-      assert File.exists?(path)
-      File.rm!(path)
-    end
+    _ = File.rm(path)
+    {"", 0} = InteractiveCmd.cmd("touch", [path])
 
-    test "paths with quotes" do
-      path = "tmp_file with ' and \".txt"
+    assert File.exists?(path)
+    File.rm!(path)
+  end
 
-      _ = File.rm(path)
-      {"", 0} = InteractiveCmd.cmd("touch", [path])
+  test "paths with quotes" do
+    path = "tmp_file with ' and \".txt"
 
-      assert File.exists?(path)
-      File.rm!(path)
-    end
+    _ = File.rm(path)
+    {"", 0} = InteractiveCmd.cmd("touch", [path])
 
-    test "setting environment" do
-      path = "tmp_file.txt"
-      env_name = "INTERACTIVE_SHELL_TEST"
+    assert File.exists?(path)
+    File.rm!(path)
+  end
 
-      # If the variable is set before the test is run, then the check
-      # afterwards will fail even though all is good
-      assert System.get_env(env_name) == nil
-      _ = File.rm(path)
+  test "setting environment" do
+    path = "tmp_file.txt"
+    env_name = "INTERACTIVE_SHELL_TEST"
 
-      {"", 0} = InteractiveCmd.cmd("sh", ["-c", "touch $#{env_name}"], env: %{env_name => path})
+    # If the variable is set before the test is run, then the check
+    # afterwards will fail even though all is good
+    assert System.get_env(env_name) == nil
+    _ = File.rm(path)
 
-      assert File.exists?(path)
-      File.rm!(path)
+    {"", 0} = InteractiveCmd.cmd("sh", ["-c", "touch $#{env_name}"], env: %{env_name => path})
 
-      assert System.get_env(env_name) == nil
-    end
+    assert File.exists?(path)
+    File.rm!(path)
 
-    test "interactive tty" do
-      path = "tmp_file.txt"
-      _ = File.rm(path)
+    assert System.get_env(env_name) == nil
+  end
 
-      {"", 0} =
-        InteractiveCmd.cmd("sh", [
-          "-c",
-          "[ -t 0 ] && echo interactive > #{path} || echo non-interactive > #{path}"
-        ])
+  test "interactive tty" do
+    path = "tmp_file.txt"
+    _ = File.rm(path)
 
-      assert File.read(path) == {:ok, "interactive\n"}
-      File.rm!(path)
-    end
+    {"", 0} =
+      InteractiveCmd.cmd("sh", [
+        "-c",
+        "[ -t 0 ] && echo interactive > #{path} || echo non-interactive > #{path}"
+      ])
 
-    test "cd option" do
-      original_dir = File.cwd!()
-      temp_dir = System.tmp_dir!()
+    assert File.read(path) == {:ok, "interactive\n"}
+    File.rm!(path)
+  end
 
-      filename = "tmp_file_in_temp.txt"
-      path = Path.join(temp_dir, filename)
-      _ = File.rm(path)
+  test "cd option" do
+    original_dir = File.cwd!()
+    temp_dir = System.tmp_dir!()
 
-      {"", 0} = InteractiveCmd.cmd("touch", [filename], cd: temp_dir)
+    filename = "tmp_file_in_temp.txt"
+    path = Path.join(temp_dir, filename)
+    _ = File.rm(path)
 
-      assert File.exists?(path)
-      assert File.cwd!() == original_dir
+    {"", 0} = InteractiveCmd.cmd("touch", [filename], cd: temp_dir)
 
-      File.rm!(path)
-    end
+    assert File.exists?(path)
+    assert File.cwd!() == original_dir
+
+    File.rm!(path)
   end
 end
