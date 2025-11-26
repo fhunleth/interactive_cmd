f566921a87c87ab3d23e05f3323863e7a46326f7
Frank Hunleth <fhunleth@troodon-software.com>
2025-11-26 11:47:12 -0500

    debug

--- test/interactive_cmd_test.exs
+++ test/interactive_cmd_test.exs
@@ -3,7 +3,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 defmodule InteractiveCmdTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: false
   doctest InteractiveCmd
 
   describe "interactive_cmd/3" do
